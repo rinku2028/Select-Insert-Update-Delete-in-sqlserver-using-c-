57f6941e6c25686817b8dea76db2e4d36a05760c
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-21 23:35:01 +0545

    Add files via upload

---
+++ simpleorder.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="simpleorder.aspx.cs" Inherits="gridview.simpleorder" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Button ID="btnview" runat="server" Text="View" OnClick="btnview_Click" />
+        <asp:GridView ID="gdview" runat="server"></asp:GridView>
+        <div>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ simpleorder.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace gridview
+{
+    public partial class simpleorder : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnview_Click(object sender, EventArgs e)
+        {
+            string dataconn = "Data Source=desktop-tb3r1mj;Initial Catalog=access;Integrated Security=True";
+            SqlConnection sqlconn = new SqlConnection(dataconn);
+            try
+            {
+                sqlconn.Open();
+                Response.Write("database connected");
+                string sql = "select id,name from info";
+                //SqlCommand sqlcommand = new SqlCommand(sql, sqlconn);
+                SqlDataAdapter sqldata = new SqlDataAdapter(sql,sqlconn);
+                DataSet data = new DataSet();
+                sqldata.Fill(data, "info");
+                gdview.DataSource = data.Tables["info"].DefaultView;
+                gdview.DataBind();
+
+               
+            }
+            catch
+            {
+                Response.Write("Database not connected");
+            }
+
+        }
+    }
+}
